91149e27ac1c6d4bdaf8c403ffe3c899ba180ac1
Dishambha Awasthi <dishambhaawasthi0131@gmail.com>
2025-07-26 08:01:20 +0530

    added questions page and questions with correct asn
    note that the first element in the list is the correct option but while displaying we will shuffle it.
    
    also added an if block

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -2,16 +2,28 @@ import 'package:flutter/material.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
-
   @override
   State<QuestionScreen> createState() {
-    return _QuestionScreenState();
+    return _QuestionScreen();
   }
 }
 
-class _QuestionScreenState extends State<QuestionScreen> {
+class _QuestionScreen extends State<QuestionScreen> {
   @override
-  Widget build(context) {
-    return Text("Question Slide");
+  Widget build(context){
+    return  SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('The Question..'),
+          const SizedBox(height: 30),
+          ElevatedButton(onPressed: () {}, child: Text('Answer 1'),),
+          ElevatedButton(onPressed: () {}, child: Text('Answer 2'),),
+          ElevatedButton(onPressed: () {}, child: Text('Answer 3'),),
+          ElevatedButton(onPressed: () {}, child: Text('Answer 4'),),
+        ],
+      ),
+    );
   }
 }
--- lib/quiz.dart
+++ lib/quiz.dart
@@ -16,12 +16,18 @@ class _QuizState extends State<Quiz> {
 
   void switchScreen() {
     setState(() {
-      activeScreen = 'question=screen';
+      activeScreen = 'question-screen';
     });
   }
 
   @override
   Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if(activeScreen == 'question-screen'){
+      screenWidget = const QuestionScreen();
+    }
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
@@ -35,9 +41,7 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: activeScreen == 'start-screen'
-              ? StartScreen(switchScreen)
-              : const QuestionScreen(),
+          child: screenWidget,
         ),
       ),
     );
